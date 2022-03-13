38af3dda83d06ea2d107bee1c462a7ad3d964e27
Haticesurumlu <71832100+Haticesurumlu@users.noreply.github.com>
2022-03-13 10:22:24 +0300

    feat:  selected seat count compute

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const container = document.querySelector('.container');
 container.addEventListener('click', function(e){
 if(e.target.classList.contains('seat')&& !(e.target.classList.contains('reserved'))){
 e.target.classList.toggle('selected');
+
+let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+console.log(selectedSeatCount);
 }
 
 });
+
