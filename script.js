0e4cf7702145cac8a16475c184cbd27a0bfc3405
Haticesurumlu <71832100+Haticesurumlu@users.noreply.github.com>
2022-03-13 10:40:43 +0300

    add count and amount

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector('.container');
+const count = document.getElementById ('count');
+const amount= document.getElementById ('amount');
 
 container.addEventListener('click', function(e){
 if(e.target.classList.contains('seat')&& !(e.target.classList.contains('reserved'))){
 e.target.classList.toggle('selected');
 
 let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-console.log(selectedSeatCount);
+
 }
 
 });
