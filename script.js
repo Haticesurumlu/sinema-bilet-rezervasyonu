3df44a33dd464a91e48223fe727412f0312607b1
Haticesurumlu <71832100+Haticesurumlu@users.noreply.github.com>
2022-03-13 11:09:19 +0300

    count the price

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ container.addEventListener('click', function(e){
 if(e.target.classList.contains('seat')&& !(e.target.classList.contains('reserved'))){
 e.target.classList.toggle('selected');
 
-let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-let price = select.value;
-count.innerText = selectedSeatCount ;
-amount.innerText = selectedSeatCount * price ; 
+calculateTotal();
 
 }
 
 });
+
+select.addEventListener('change',function(e){
+    calculateTotal();
+});
+
+function calculateTotal(){
+    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+let price = select.value;
+count.innerText = selectedSeatCount ;
+amount.innerText = selectedSeatCount * price ; 
+}
 
